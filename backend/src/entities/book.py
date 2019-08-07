74fc0e28ef376ce8c4816e7ecd36726901b91def
Artyom Morozov <47092586+Sviktam1@users.noreply.github.com>
2019-08-06 23:28:28 -0400

    Added Angular project

--- backend/src/entities/book.py
+++ backend/src/entities/book.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 
 from sqlalchemy import Column, String, Integer, Date
-
+from marshmallow import Schema, fields
 from .entity import Entity, Base
 
 
@@ -18,3 +18,11 @@ class Book(Entity, Base):
         Entity.__init__(self, created_by)
         self.title = title
         self.author = author
+
+class BookSchema(Schema):
+    id = fields.Number()
+    title = fields.Str()
+    author = fields.Str()
+    created_at = fields.DateTime()
+    updated_at = fields.DateTime()
+    last_updated_by = fields.Str()
--- backend/src/main.py
+++ backend/src/main.py
@@ -1,28 +1,45 @@
 # coding=utf-8
+from flask import Flask, jsonify, request
 
 from .entities.entity import Session, engine, Base
-from .entities.book import Book
+from .entities.book import Book, BookSchema
 
-# generate database schema
+# creating the Flask application
+app = Flask(__name__)
+
+# if needed, generate database schema
 Base.metadata.create_all(engine)
 
-# start session
-session = Session()
 
-# check for existing data
-books = session.query(Book).all()
+@app.route('/books')
+def get_books():
+    # fetching from the database
+    session = Session()
+    book_objects = session.query(Book).all()
 
-if len(books) == 0:
-    # create and persist dummy book
-    python_book = Book("SQLAlchemy Book", "SQLAlchemy devs", "script")
-    session.add(python_book)
-    session.commit()
+    # transforming into JSON-serializable objects
+    schema = BookSchema(many=True)
+    books = schema.dump(book_objects)
+
+    # serializing as JSON
     session.close()
+    return jsonify(books.data)
 
-    # reload books
-    books = session.query(Book).all()
 
-# show existing books
-print('### books:')
-for book in books:
-    print(f'({book.id}) {book.title} - {book.author}')
+@app.route('/books', methods=['POST'])
+def add_book():
+    # mount book object
+    posted_book = BookSchema(only=('title', 'author'))\
+        .load(request.get_json())
+
+    book = Book(**posted_book.data, created_by="HTTP post request")
+
+    # persist book
+    session = Session()
+    session.add(book)
+    session.commit()
+
+    # return created book
+    new_book = BookSchema().dump(book).data
+    session.close()
+    return jsonify(new_book), 201
