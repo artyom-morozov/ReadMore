6db4b67d9b185591a0fcd9fd628c8721062e4b5e
Artyom Morozov <47092586+Sviktam1@users.noreply.github.com>
2020-01-22 23:38:41 -0500

    Added requirements.txt

--- backend/django-read/readMore/read/views.py
+++ backend/django-read/readMore/read/views.py
@@ -34,6 +34,28 @@ class MyRatingsView(ListView):
 
 # Add book rating
 def add_rating(request): #, book_id, user_id):
+    # create a form instance and populate it with data from the request:
+        # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = NameForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect('/thanks/')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = NameForm()
+
+    return render(request, 'name.html', {'form': form})
+
+    return render(request, 'name.html', {'form': form})
+
+
+
     if request.method == "POST":
         # rating = form.cleaned_data['value']
         # book = form.cleaned_data['book']
